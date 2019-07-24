fc90e543fb49ac6e71611ded216aca1f45bd8d13
zforgo <zforgo@gmail.com>
2019-07-24 17:55:55 +0200

    rename file

---
+++ scratchpad.js
@@ -0,0 +1,6 @@
+//const possibilities = [...Array(9).keys()].map(i=>++i);
+const possibilitiesBoard = [...Array(9)].map(
+        () => [...Array(9)].map(()=> [...Array(9).keys()].map(i=>++i))
+    );
+//console.log(possibilities)
+console.log(possibilitiesBoard)
--- test.js
+++
@@ -1,4 +0,0 @@
-const possibilities = Array.from(Array(9).keys(i=> ++i));
-const possibilities2 = [...Array(9).keys()].map(i=>++i);
-console.log(possibilities)
-console.log(possibilities2)
