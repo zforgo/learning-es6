cc7683d99059d7fe35d863f797871f0573ffc11a
zforgo <zforgo@gmail.com>
2019-08-19 14:59:43 +0200

    minor test refactoring

--- test/possibilites.test.js
+++ test/possibilites.test.js
@@ -1,22 +1,29 @@
 const each = require('jest-each').default;
 const solver = require('../lib/solver');
 const boards = require('./data/boards');
-const possibilities = require('./data/possibilities');
 
-each(possibilities.by_position).describe('check possibilities by position',
-    (position, expected) => {
-        test(`returns ${Array.isArray(expected) && expected.length === 0 ? 'empty' : `${expected}`} possibilities for position: ${position}`, () => {
-            expect(solver.getPossibility(boards.board, position)).toStrictEqual(expected);
-        });
-    }
-);
 
 describe('check possibilities', () => {
+    const possibilities = require('./data/possibilities');
     test('whole table', () => {
         expect(solver.getPossibilities(boards.board)).toStrictEqual(possibilities.whole_table);
     })
-    // test('coordinates', () => {
-    //     expect(solver.calculateIndices(0)).toStrictEqual(true);
+    each(possibilities.by_position).describe('check possibilities by position',
+        (position, expected) => {
+            test(`returns ${Array.isArray(expected) && expected.length === 0 ? 'empty' : `${expected}`} possibilities for position: ${position}`, () => {
+                expect(solver.getPossibility(boards.board, position)).toStrictEqual(expected);
+            });
+        }
+    );
+});
 
-    // })
+describe('indicies', () => {
+    const indices = require('./data/indices');
+    each(indices).describe('check indices for whole table',
+        (position, expected) => {
+            test(`should return ${expected} indices for given position: ${position}`, () => {
+                expect(solver.calculateIndices(position)).toStrictEqual(expected);
+            });
+        }
+    );
 });
